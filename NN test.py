ec863a191583708786b01dd518b982f502d5ec1f
CCC101 <101533240+linbiaotongzhiyongyuanjiankang@users.noreply.github.com>
2022-08-25 19:43:27 +0800

    Add files via upload

--- NN test.py
+++ NN test.py
@@ -6,17 +6,23 @@ import torch.nn as nn
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 
-x = torch.randn(64,64)
+x = torch.randn(64,16)
 
 class train_model(nn.Module):
     def __init__(self): # 相当于定义self自身的_init_文档
         super(train_model,self).__init__()  # 定义网络，继承自身，引入由nn.module类定义的网络层
-        self.linear_0 = nn.Linear(64,64)
-        self.sigmoid = nn.Sigmoid()
-        self.linear_1 = nn.Linear(64,16)
-        self.relu = nn.ReLU()
+        self.linear_0 = nn.Linear(16,32)
+        self.linear_1 = nn.Linear(32,16)
         self.linear_2 = nn.Linear(16,1)
-        self.linear_3 = nn.Linear(64,1)
+        # linear sets
+        self.linear_3 = nn.Linear(16,1)
+        # linear sets
+        self.judge_0 = nn.Linear(1,16)
+        self.judge_1 = nn.Linear(16,1)
+        # iudge sets
+        self.sigmoid = nn.Sigmoid()
+        self.relu = nn.ReLU()
+        # spec sets
 
     def forward(self,x):
         x_0 = torch.clone(x)
@@ -26,7 +32,12 @@ class train_model(nn.Module):
         x = self.relu(x)
         x = self.linear_2(x)
         x_1 = self.linear_3(x_0)
-        goal_out = torch.add(x,x_1)
+        x_1 = self.relu(x_1)
+        goal = torch.add(x,x_1)
+        goal = self.judge_0(goal)
+        goal = self.relu(goal)
+        goal = self.judge_1(goal)
+        goal_out = self.sigmoid(goal)
         return goal_out
 
 
@@ -37,9 +48,16 @@ torch.onnx.export(
     net_model,
     x,
     'net_work test.onnx',
+    export_params=False,
     input_names=['x'],
     output_names=['goal_out'],
-    dynamic_axes={'x':{0:'sample_batch'},'goal_out':{0:'goal_batch'}}
+
 )
 
 print(net_model)
+
+'''
+In PyTorch, linear computing is defined as goal.t() = weight[infeature,output].t() * trigger[sample_batch,infeature].t() + bias[sample_batch,output].t()
+That's why we would find linear weights are displaying as being transposed in Netron
+'''
+
